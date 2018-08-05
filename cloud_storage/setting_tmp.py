014265ddead76c4b3b11acc9af74d3aa874d70aa
yoneda hayato <yhayato1320@gmail.com>
2018-08-05 22:25:50 +0900

    added setting

--- cloud_storage/setting_tmp.py
+++ cloud_storage/setting_tmp.py
@@ -1,3 +1,5 @@
+import os
+
 # google api key
 api_key_json_path = ""
 
@@ -6,3 +8,9 @@ bucket_name = ""
 
 # log
 log_save_path = ""
+
+# server port
+port = 8090
+
+# upload file path
+upload_file_path =
