d50f77add10a37d1d1a664339d0684f34736b73e
yoneda hayato <yhayato1320@gmail.com>
2018-08-05 22:27:11 +0900

    fixed something

--- cloud_storage/uploader.py
+++ cloud_storage/uploader.py
@@ -10,6 +10,9 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=setting.api_key_json_path
 # log
 logger = log.logger
 
+# upload file path
+upload_file_path = setting.upload_file_path
+
 class Google_Cloud_Storage:
     def __init__(self):
         self.client = storage.Client()
@@ -28,14 +31,31 @@ class Google_Cloud_Storage:
         msg = "[Google_Cloud_Storage:update]: {}"
 
         try:
-            blob = self.bucket.blob(file_path)
-            blob.upload_from_filename(file_path)
+            file_name = os.path.basename(file_path)
+            dir_name = file_path.split("/")[-2]
+            blob = self.bucket.blob("{}/{}".format(dir_name, file_name))
+            blob.upload_from_filename("{}/{}".format(upload_file_path, file_name))
         except Exception as e:
             err_msg = msg.format("fail to upload {}, {}".format(file_path, e))
             self.logging_error(err_msg)
             raise Exception(err_msg)
         else:
             sccs_msg = msg.format("success to update {}".format(file_path))
+            self.logging_info(sccs_msg)
+
+    def download(self, file_path):
+        msg = "[Google_Cloud_Storage:download]: {}"
+
+        try:
+            file_name = os.path.basename(file_path)
+            blob = self.bucket.blob(file_path)
+            blob.download_to_filename("{}/{}".format(upload_file_path, file_name))
+        except Exception as e:
+            err_msg = msg.format("fail to download {}, {}".format(file_path, e))
+            self.logging_error(err_msg)
+            raise Exception(err_msg)
+        else:
+            sccs_msg = msg.format("success to download {}".format(file_path))
             self.logging_info(sccs_msg)
 
     def test_print(self):
