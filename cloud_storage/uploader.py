c36c54e226a3ab876cf53e19c680f7c9ec53e1af
yoneda <yoneda>
2019-09-19 11:00:05 +0900

    一般公開に設定する機能を追加

--- cloud_storage/uploader.py
+++ cloud_storage/uploader.py
@@ -11,18 +11,12 @@ class Uploader:
         self.bucket_name = bucket_name
         self.bucket = self.client.get_bucket(self.bucket_name)
 
-    def upload(self, file_path, public=False):
+    def upload(self, local_path, gcp_path, public=False):
         msg = "[Google_Cloud_Storage:update]: {}"
 
         try:
-            file_name = os.path.basename(file_path)
-            dir_name = file_path.split("/")[-2]
-            blob = self.bucket.blob("{}/{}".format(dir_name, file_name))
-
-            if public:
-                blob.make_public()
-
-            blob.upload_from_filename("{}/{}".format(upload_file_path, file_name))
+            blob = self.bucket.blob(gcp_path)
+            blob.upload_from_filename(local_path)
         except Exception as e:
             err_msg = msg.format("fail to upload {}, {}".format(local_path, e))
             logger.exception(err_msg)
