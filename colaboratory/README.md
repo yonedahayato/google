85247d8c9286cad0020677a83d0192a1ccadb07f
yoneda <yoneda>
2019-09-23 22:52:00 +0900

    最初のスクリプト追加

---
+++ colaboratory/README.md
@@ -0,0 +1,9 @@
+# 目的
+- Google ColaboratoryをAPIのようにして学習を行いたい
+    - 参考 : [Google Colaboratoryを定期実行する方法について考える](https://qiita.com/Fortinbras/items/4cfa9269af2ab8d1d4d5)
+
+# スクリプト
+|スクリプト名|概要|
+|---|---|
+|schedule.py|セッションを切らさないよう「常時稼働させる」方向で90分のセッション切れを回避する。```colab上で起動想定```|
+|exec_gcolab.py|ローカルPC上にてSelenumでGoogle Driveにログインして、Google Colabratoryのファイルを開いて一番上のセルの実行ボタンをクリックする|
---
+++ colaboratory/exec_gcolab.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+from setting import (
+    COLAB_URL,
+    GOOGLE_PASS
+    GOOGLE_ID,
+)
+
+driver = webdriver.Chrome()
+url = COLAB_URL
+
+login_id = GOOGLE_ID
+pas = GOOGLE_PASS
+
+# ページを開く
+driver.get(url)
+
+time.sleep(5)
+# ログイン画面:userid
+driver.find_element_by_xpath("//*[@id='identifierId']").send_keys(login_id)
+driver.find_element_by_class_name('CwaK9').click()
+time.sleep(2)
+
+# ログイン画面:pwd
+driver.find_element_by_xpath("//*[@id='password']/div[1]/div/div[1]/input").send_keys(pas)
+driver.find_element_by_xpath("//*[@id='passwordNext']").click()
+time.sleep(10)
+
+# Google Colab
+print(driver.current_url)
+driver.find_element_by_class_name('cell-execution').click() #セルの実行
