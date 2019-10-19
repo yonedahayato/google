e0c9e4e890a88617fc0e698bb3c629899cf41463
yoneda <yoneda>
2019-10-19 21:37:54 +0900

    docker exe 修正

--- colaboratory/docker_exe.sh
+++ colaboratory/docker_exe.sh
@@ -1,2 +1,4 @@
+docker build -t exec_gcolab_lib -f ./Dockerfile_lib .
 docker build -t exec_gcolab .
-docker run -it --rm exec_gcolab python3 exec_gcolab.py
+docker run -v $PWD/log:/home/colaboratory/log -it --rm exec_gcolab python3 exec_gcolab.py
+docker rmi exec_gcolab
