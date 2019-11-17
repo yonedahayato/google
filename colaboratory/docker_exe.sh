83038171ca700878333044658ad209264a39f051
yoneda <yoneda>
2019-11-17 23:23:30 +0900

    docker 環境を整えることができず、断念。とりあえず、作業状況をpushする

--- colaboratory/docker_exe.sh
+++ colaboratory/docker_exe.sh
@@ -1,4 +1,9 @@
-docker build -t exec_gcolab_lib -f ./Dockerfile_lib .
+# docker build -t exec_gcolab_lib -f ./Dockerfile_lib .
 docker build -t exec_gcolab .
-docker run -v $PWD/log:/home/colaboratory/log -it --rm exec_gcolab python3 exec_gcolab.py
+docker run \
+  -v $PWD/log:/home/colaboratory/log \
+  -v /Users/yonedahayato/Library/Application\ Support/Google/Chrome:/home/colaboratory/userdata \
+  -p 9222:9222 \
+  -it --rm exec_gcolab /bin/bash
+# docker run -v $PWD/log:/home/colaboratory/log -it --rm exec_gcolab python3 exec_gcolab.py
 docker rmi exec_gcolab
