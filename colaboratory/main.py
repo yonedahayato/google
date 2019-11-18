ef5bb6b8b7c7877c0cd3c7c45ea8370cde6631df
yoneda <yoneda>
2019-11-18 09:02:01 +0900

    main.py修正

--- colaboratory/main.py
+++ colaboratory/main.py
@@ -1,1 +1,9 @@
+import os
 from exec_gcolab import main as exec_gcolab
+
+def main():
+    url = os.environ["COLAB_URL"]
+    login_id = os.environ["GOOGLE_ID"]
+    pas = os.environ["GOOGLE_PASS"]
+
+    exec_gcolab(url, login_id, pas)
